e78bffad3500d525c90924fc5fede350238b3504
santhoshpog <40847670+santhoshpog@users.noreply.github.com>
2021-05-08 18:53:58 +0530

    Update logcatcher.py

--- logcatcher.py
+++ logcatcher.py
@@ -1,6 +1,10 @@
 with open('abc.log','r') as f:
     for line in f:
         line=line.strip()
-        tim,msg,info=line.split()
-        if 'error' in msg or 'warning' in msg:
-            print(line)
+        try:
+            new=line.split()
+            if 'error' in new[1]:
+                print(line)
+        except Exception as e:
+            print(e)
+
